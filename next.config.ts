5fedb1ab2d281d4093c7313ec77f39e9758bcac8
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-08-11 02:50:24 +0300

    Small fix regarding fetch issues

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type { NextConfig } from "next";
+import dns from 'node:dns'
+import { setDefaultAutoSelectFamilyAttemptTimeout } from 'node:net'
+import { setGlobalDispatcher, Agent } from 'undici'
+
+setGlobalDispatcher(new Agent({ connect: { family: 4 } }))
+setDefaultAutoSelectFamilyAttemptTimeout(100)
+dns.setDefaultResultOrder('ipv4first')
 
 const nextConfig: NextConfig = {
   /* config options here */
