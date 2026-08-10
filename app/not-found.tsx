5145f72b9f206402322cad9835ee21422ba163a3
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-08-11 00:29:11 +0300

    Added a not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Page Not Found | YourAppName',
+  description: 'The page you are looking for does not exist.',
+};
+
+export default function NotFound() {
+    return (
+        <p>Page Not Found</p>
+    );
+}
