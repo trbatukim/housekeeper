42ae8b2182ab0f39819d31bdd3ce2d28720bfba7
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-08-17 20:48:45 +0300

    added a welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   const { data: { user } } = await supabase.auth.getUser()
 
   if (!user) {
-    redirect('/login')
+    redirect('/welcome')
   }
 
   return (
---
+++ app/welcome/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default async function WelcomePage() {
+    return (
+        <div className="container">
+            <h1 className="titleRow">
+                <span className="title">Welcome to</span> <span className="logo">HouseKeeper</span>
+            </h1>
+            <div className="contentBox">
+                <div className="buttonRow">
+                    <Link href="/signup" className="link">Sign-Up</Link>
+                    <Link href="/login" className="link">Login</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
